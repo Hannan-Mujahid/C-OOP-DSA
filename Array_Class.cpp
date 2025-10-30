ec3d9869518fa30d205d48cfee2a9b81fd0d7b27
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-31 03:46:17 +0500

    update

---
+++ Array_Class.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+    for (int i = 0; i<5; i++){
+        cout<<"Enter array element "<<i+1 <<": " <<endl;
+        cin>>arr[i];    
+    }
+}
