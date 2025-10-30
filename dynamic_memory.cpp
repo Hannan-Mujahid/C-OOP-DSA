75525723216714b47e64882455acf2f4a78bcb03
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-31 03:41:15 +0500

    update

--- dynamic_memory.cpp
+++ dynamic_memory.cpp
@@ -2,24 +2,10 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n; // user se size lo
+int arr[10];
+for(int i=0;i<10;i++){
+    cout<<"enter array "<<endl;
+cin>>arr[i];
+}
 
-    // Dynamic memory allocation
-    int *arr = new int[n];  
-
-    cout << "Enter " << n << " numbers:\n";
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    cout << "You entered: ";
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-
-    // Free memory 
-    delete[] arr;
-
-    return 0;
 }
