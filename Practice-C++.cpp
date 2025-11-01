6514c949bd634a16088488d6ff35cebb2af4ebfb
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-01 22:50:26 +0500

    update

---
+++ Practice-C++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    int n= 24;
+    if (n>= 0){
+        cout<<"N is positive"<<endl;
+    }else{
+        cout<<"N is negative"<<endl;
+    }
+};
