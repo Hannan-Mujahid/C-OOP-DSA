14ed1cb59a91f966d487fa9be5a5c5525d864790
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-02 14:40:54 +0500

    update

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -118,7 +118,7 @@ int main(){
     int  find = 0;
     int sum= 0;
     for (int i=1; i<=n; i++){
-        if (i % 2 !=0 ){
+        if (i % 2 ==0 ){
             cout<<i<<" is even number"<<endl;
             sum= i+i;
         }else {
