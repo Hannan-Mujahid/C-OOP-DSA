47d54d8d33acfb5532995a5e16f9f0ef052323e0
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-02 19:51:19 +0500

    update

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -228,3 +228,20 @@ int main(){
     return 0;
 }
 */
+// sum of all numbers from 1 to n which are divisible by 3.
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int sum=0;
+    for (int i=1; i<=n; i++){
+        if (i %4 == 0){
+            cout<<i<<" is divisible by 3 "<<endl;
+            sum +=i;
+        }
+    }
+    cout<<"Sum is: "<<sum<<endl;
+}
