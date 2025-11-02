5606c6ebdf32d96311cf32737f8cde0c9dbc1cd1
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-02 14:12:30 +0500

    update

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -109,3 +109,22 @@ int main(){
     return 0;
 }
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n =10;
+    int  find = 0;
+    int sum= 0;
+    for (int i=1; i<=n; i++){
+        if (i % 2 !=0 ){
+            cout<<i<<" is even number"<<endl;
+            sum= i+i;
+        }else {
+            cout<<i<<"is odd number"<<endl;
+        }
+    }
+    cout<<"Sum is: " <<sum<<endl;
+        return 0;
+}
