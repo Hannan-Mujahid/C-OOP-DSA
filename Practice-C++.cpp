6e1c518f5b41900687b4b4c9c8dd7c563cfa759f
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-03 18:30:46 +0500

    update:

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -279,3 +279,19 @@ int main(){
     return 0;
 }
 */
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+
+    int n=26;
+    for (int i=0; i<n; i++){
+        char ch='A';
+        for (int j=0; j<n; j++){
+            cout<<ch<<" ";
+            ch++;
+        } 
+        cout<<endl;
+   }
+       return 0;
+}
