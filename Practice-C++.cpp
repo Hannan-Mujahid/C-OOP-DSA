ae7de6344cad453934053b142319d21deae4d87a
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-04 18:52:43 +0500

    update

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -429,10 +429,18 @@ int main(){
 using namespace std;
 int main(){ 
 
-    for (int i=0; i<5; i++){
-        for (int j=i+1; j>0; j--){
-            cout<<j<<" ";
-      }
+    // for (int i=0; i<5; i++){
+    //     for (int j=i+1; j>0; j--){
+    //         cout<<j<<" ";
+    //   }
+    //     cout<<endl;
+    // }
+
+
+    for (int i=1; i<5; i++){
+        for (int j=i; j>=1; j--){
+            cout<<j<< " ";
+        }
         cout<<endl;
     }
     
