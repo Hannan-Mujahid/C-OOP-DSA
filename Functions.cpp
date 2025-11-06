e658f0160a8215e3627d790a5161384ec2e74749
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-06 09:19:14 +0500

    update

--- Functions.cpp
+++ Functions.cpp
@@ -12,13 +12,11 @@ using namespace std;
 // }
 
 int factorial(int n){
-    //  int fact =1;
-    //  for (int i=1; i<=n; i++){
-    //     fact =fact*i;
-    //  }
-    //  return fact;
-  
-
+     int fact =1;
+     for (int i=1; i<=n; i++){
+        fact =fact*i;
+     }
+     return fact;
 }
 
 int main(){
