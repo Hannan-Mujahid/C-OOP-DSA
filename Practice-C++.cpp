868e74c97a0b19fc302ab093224c3d84ee49c732
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-09 12:50:37 +0500

    update

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -531,7 +531,15 @@ int main(){
 #include<string>
 using namespace std;
 int main(){
-
-
+    
+    int num[5]= {5,15, 22,1,-15};
+    int size = 6;
+    int smallest= INT_FAST8_MAX;
+    for (int i=0; i<size; i++){
+        if (num[i]< smallest){
+            smallest = num[i];
+        }
+}
+cout<<"smallest: " <<smallest<<endl;
     return 0;
 }
