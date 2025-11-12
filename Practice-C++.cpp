b7e7b14531b037c0f0142e375b4e2e59bb7a2b15
Hannan Mujahid <hannimujahid37@gmail.com>
2025-11-12 14:02:49 +0500

    update

--- Practice-C++.cpp
+++ Practice-C++.cpp
@@ -600,4 +600,27 @@ int main (){
 }
 */
 
+#include<iostream>
+#include<string>
+using namespace std;
+void reverseArray(int arr[], int size){
+    int start= 0, end = size-1;
+    while (start < end){
+       swap ( arr[start],arr[end] );
+       start++;
+       end--;
+    }
+}
+int main(){
+
+    int arr[] = {4,2,7,8,1,2,5};
+    int sz = 7;
+    reverseArray(arr,sz);
+    for (int i=0; i<sz; i++){
+        cout<<arr[i]<<" ";
+
+    }
+    cout<<endl;
+    return 0;
+}
 
